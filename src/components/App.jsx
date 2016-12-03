a92d5c21e1c1524a40f7320ec1905aab183fb4ff
Brendan Frost <brendangfrost@gmail.com>
2016-12-02 20:25:45 -0600

    Passed video list, entry, and player tests weee

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentVideo: null,
+      videos: []
+    }
+  }
+  render(){
 
+    return <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer />
+      </div>
+      <div className="col-md-5">
+        <VideoList video={this.props.videos}/>
+      </div>
+    </div>
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
